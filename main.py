2eeb208817dc1e2ef55229f066d6161baeb71ca6
Iulian <iulian@gmail.com>
2026-01-10 18:52:36 +0100

    HTML Form and Main files

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
 
-app = Falsk(__name__)
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    if "username" in session:
+        return redirect(url_for('dashboard.html'))
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
